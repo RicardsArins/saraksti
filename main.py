12969510f9655bedd0ed641d6b7e7a2685665542
Ričards Matīss Matīss <94109259+RicardsArins@users.noreply.github.com>
2021-12-09 09:41:32 +0000

    add print

---
+++ main.py
@@ -0,0 +1,9 @@
+print("kuru no uzdevumienj velies realizēt")
+print("izvēlies ciparu")
+print("1 - pirmo uzdevumu")
+print("2 - otro uzdevumu")
+print("3 - trešo uzdevumu")
+print("____________________")
+print("Tu esi izvēlējies uzdevuma nr.")
+
+
